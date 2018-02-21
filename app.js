812b40615aeb3d85ecae7fdd70d5855ce064fdb4
clowery <clowery52916@gmail.com>
2018-02-21 16:08:19 -0500

    adding dotenv

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI); 
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
